47756d7994e8ab39120961406d6d330d70218654
gigaherz <gigaherz@gmail.com>
2023-04-06 18:14:14 +0200

    Fix items inheriting the wrong vanilla attributes.

--- src/main/java/dev/gigaherz/jsonthings/things/items/FlexPickaxeItem.java
+++ src/main/java/dev/gigaherz/jsonthings/things/items/FlexPickaxeItem.java
@@ -62,7 +62,7 @@ public class FlexPickaxeItem extends PickaxeItem implements IFlexItem
         for (EquipmentSlot slot1 : EquipmentSlot.values())
         {
             attributeModifiers.computeIfAbsent(slot1, key -> ArrayListMultimap.create())
-                    .putAll(super.getAttributeModifiers(EquipmentSlot.CHEST, ItemStack.EMPTY));
+                    .putAll(super.getAttributeModifiers(slot1, ItemStack.EMPTY));
         }
     }
 
